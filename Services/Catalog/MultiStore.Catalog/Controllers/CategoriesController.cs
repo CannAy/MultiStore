784c68f677c8e31a72b9b7840c72c351c14c0829
Can Ay <mrtcn.ay@gmail.com>
2024-09-24 16:22:36 +0300

    github test

---
+++ Services/Catalog/MultiStore.Catalog/Controllers/CategoriesController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MultiStore.Catalog.Dtos.CategoryDtos;
+using MultiStore.Catalog.Services.CategoryServices;
+
+namespace MultiStore.Catalog.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriesController : ControllerBase
+    {
+        private readonly ICategoryService _categoryService;
+
+        public CategoriesController(ICategoryService categoryService)
+        {
+            _categoryService = categoryService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> CategoryList()
+        {
+            var values = await _categoryService.GetAllCategoryAsync();
+            return Ok(values);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetCategoryById(string id)
+        {
+            var values = await _categoryService.GetByIdCategoryAsync(id);
+            return Ok(values);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateCategory(CreateCategoryDto createCategoryDto)
+        {
+            await _categoryService.CreateCategoryAsync(createCategoryDto);
+            return Ok("Kategori başarıyla eklendi");
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> DeleteCategory(string id)
+        {
+            await _categoryService.DeleteCategoryAsync(id);
+            return Ok("Kategori başarıyla silindi");
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> UpdateCategory(UpdateCategoryDto updateCategoryDto)
+        {
+            await _categoryService.UpdateCategoryDtoAsync(updateCategoryDto);
+            return Ok("Kategori başarıyla güncellendi");
+        }
+    }
+}
